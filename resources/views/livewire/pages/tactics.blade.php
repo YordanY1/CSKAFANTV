6e041fd75bd0101c2d57fd47340420419acbd90e
YordanYordanov <yordanyordanov0104@gmail.com>
2025-07-26 12:09:26 +0300

    Update tactics.blade.php

--- resources/views/livewire/pages/tactics.blade.php
+++ resources/views/livewire/pages/tactics.blade.php
@@ -72,16 +72,14 @@
                 <div>
                     <label class="block text-sm font-medium text-gray-600 mb-1">Избери играч:</label>
                     <select x-model="selectedPlayerId" @change="onPlayerSelected" :disabled="tool !== null"
-                        class="border ...">
-                        <option value="">-- Избери играч --</option>
-
+                        class="border border-gray-300 rounded-lg p-2 text-sm text-gray-700 bg-white shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition w-full max-w-sm">
+                        <option :value="null" x-show="true">-- Избери играч --</option>
                         <template x-for="player in players" :key="player.id">
                             <option :value="player.id"
                                 x-text="player.name + (player.position ? ' (' + player.position + ')' : '') + (player.number ? ' #' + player.number : '')">
                             </option>
                         </template>
                     </select>
-
                 </div>
             </div>
 
