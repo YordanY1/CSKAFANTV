22adaffd557d93efde355d21c505f1502dc4c0c7
YordanYordanov <yordanyordanov0104@gmail.com>
2025-08-07 20:11:45 +0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -223,7 +223,7 @@ window.tacticBoard = function () {
                         x: 0,
                         y: 75,
                         width: 70,
-                        text: player.name.split(" ")[1] || player.name,
+                        text: player.name.split(" ").pop(),
                         fontSize: 17,
                         fontFamily: "Calibri",
                         fill: "white",
