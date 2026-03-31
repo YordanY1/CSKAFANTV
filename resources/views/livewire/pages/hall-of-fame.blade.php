1cdf6b19fb2b26c66d57491ebe554700ec038670
YordanYordanov <yordanyordanov0104@gmail.com>
2026-04-01 01:14:20 +0300

    Update hall-of-fame.blade.php

--- resources/views/livewire/pages/hall-of-fame.blade.php
+++ resources/views/livewire/pages/hall-of-fame.blade.php
@@ -8,7 +8,7 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10">
                 @forelse ($awards->filter(fn($a) => $a->player) as $award)
                     @php
-                        $monthName = \Carbon\Carbon::createFromDate($award->year, $award->month)
+                        $monthName = \Carbon\Carbon::createFromDate($award->year, $award->month, 1)
                             ->locale('bg')
                             ->translatedFormat('F Y');
                     @endphp
